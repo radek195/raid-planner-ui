16c1ee77b57ab13e6598749df7810ae1dfbbd65b
radek195 <radoslaw.parol95@gmail.com>
2024-11-10 18:45:11 +0100

    project init vite react tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1 className="text-4xl font-bold underline">
+                Hello tailwind!
+            </h1>
+        </>
+    )
+}
+
+export default App
